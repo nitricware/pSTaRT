ab6c7c1e12f8c0249f573cbaa2d7814fc6b4ade9
Kurt Höblinger <office@nitricware.com>
2020-01-13 17:50:51 +0100

    translation fixes and icon

---
+++ pSTaRT/Questionnaire.swift
@@ -0,0 +1,30 @@
+//
+//  Questionnaire.swift
+//  pSTaRT
+//
+//  Created by Kurt Höblinger on 15.11.19.
+//  Copyright © 2019 Kurt Höblinger. All rights reserved.
+//
+
+import UIKit
+
+class Questionnaire: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ pSTaRT/ScannerView.swift
@@ -0,0 +1,30 @@
+//
+//  ScannerView.swift
+//  pSTaRT
+//
+//  Created by Kurt Höblinger on 07.12.19.
+//  Copyright © 2019 Kurt Höblinger. All rights reserved.
+//
+
+import UIKit
+
+class ScannerView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
